a28689d90aa274f5c9bbd361e0ee0c27cae42ea9
Simon Neidhart <simon.neidhart@hotmail.com>
2025-03-13 08:14:08 +0100

    add book and library according to exercise

--- IM_P2/Program.cs
+++ IM_P2/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using IM_P2;
+
 Console.WriteLine("Hello, World!");
+
+var hp1 = new Book("Harry Potter und der Stein der Weisen", "J.K. Rowling", DateOnly.Parse("2000-01-01"));
+var orwell = new Book("1984", "George Orwell", DateOnly.Parse("2047-07-10"));
+
+Console.WriteLine(hp1);
+Console.WriteLine(orwell);
+
+Library library = new Library();
+library.AddBook(hp1);
+library.AddBook(orwell);
+
+foreach (var book in library.GetBooks())
+{
+    Console.WriteLine(book);
+}
