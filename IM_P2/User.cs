057deacda71f39aeec897406fd80895ce4259140
Simon Neidhart <simon.neidhart@hotmail.com>
2025-03-30 15:10:12 +0200

    refactoring: to auto property

--- IM_P2/User.cs
+++ IM_P2/User.cs
@@ -2,15 +2,13 @@ namespace IM_P2;
 
 public class User
 {
-    private string _username;
-
     private string _password;
 
     public User(string username, string password)
     {
-        _username = username;
+        Username = username;
         _password = password;
     }
 
-    public string Username => _username;
+    public string Username { get; }
 }
