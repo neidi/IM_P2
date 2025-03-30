58b9fedfd6c6e2e93d61ff4bc3a1b6f0c36619e9
Simon Neidhart <simon.neidhart@hotmail.com>
2025-03-30 14:50:30 +0200

    fix compile errors in BankAccountTest by implementing constructor and properties in BankAccount

--- IM_P2/BankAccount.cs
+++ IM_P2/BankAccount.cs
@@ -5,7 +5,16 @@ public class BankAccount
     public string _accountHolderName;
     public Address _accountHolderAddress;
     public decimal _balance;
-    
+
+    public BankAccount(string accountHolderName, Address address)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object Balance { get; set; }
+    public object AccountHolderName { get; set; }
+    public object AccountHolderAddress { get; set; }
+
     public void Withdraw(decimal amount)
     {
         _balance -= amount;
