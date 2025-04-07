9056fa7859d2dd95b7945af802a69510d02dba73
Simon Neidhart <simon.neidhart@hotmail.com>
2025-04-07 19:37:16 +0200

    fix tests

--- IM_P2.Tests/DecimalExtensionsTests.cs
+++ IM_P2.Tests/DecimalExtensionsTests.cs
@@ -3,9 +3,9 @@ namespace IM_P2.Tests;
 public class DecimalExtensionsTests
 {
     [Theory]
-    [InlineData(10M, "CHF", "10.00 CHF")]
-    [InlineData(14M, "EUR", "14.00 EUR")]
-    [InlineData(68M, "USD", "68.00 USD")]
+    [InlineData(10, "CHF", "10.00 CHF")]
+    [InlineData(14, "EUR", "14.00 EUR")]
+    [InlineData(68, "USD", "68.00 USD")]
     public void ToCurrencyString_ShouldReturnFormattedString(decimal amount, string currency, string expected)
     {
         // Act
