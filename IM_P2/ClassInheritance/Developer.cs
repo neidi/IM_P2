da3628869ab0ed9c301aa7bedb5a27d81ac6969a
Simon Neidhart <simon.neidhart@hotmail.com>
2025-04-29 22:12:14 +0200

    complete XML doc by adding one to every method

--- IM_P2/ClassInheritance/Developer.cs
+++ IM_P2/ClassInheritance/Developer.cs
@@ -3,19 +3,26 @@ namespace IM_P2.ClassInheritance;
 /// <summary>
 /// Employee in the role of a developer.
 /// </summary>
-/// <param name="name"><inheritdoc cref="Employee"/></param>
-/// <param name="id"><inheritdoc cref="Employee"/></param>
+/// <inheritdoc cref="Employee"/>
 /// <param name="overtimeHours">
 /// The overtime hours the developer has worked. The method CalculateBonus()
 /// is dependent on the overtime hours worked.
 /// </param>
 public class Developer(string name, int id, int overtimeHours) : Employee(name, id)
 {
+    /// <summary>
+    /// Developer get 100 for each overtime hour as a bonus.
+    /// </summary>
+    /// <returns>The overtime hourly rate * 100</returns>
     protected override decimal CalculateBonus()
     {
         return 100m * overtimeHours;
     }
 
+    /// <summary>
+    /// Displays the information of the developer.
+    /// Adds developer-specific information to the base class method.
+    /// </summary>
     public override void DisplayInfo()
     {
         // Aufruf der Methode der Basisklasse
--- IM_P2/ClassInheritance/Manager.cs
+++ IM_P2/ClassInheritance/Manager.cs
@@ -3,15 +3,22 @@ namespace IM_P2.ClassInheritance;
 /// <summary>
 /// Employee in the role of a manager.
 /// </summary>
-/// <param name="name"><inheritdoc cref="Employee"/></param>
-/// <param name="id"><inheritdoc cref="Employee"/></param>
+/// <inheritdoc cref="Employee"/>
 public class Manager(string name, int id) : Employee(name, id)
 {
+    /// <summary>
+    /// Managers get 2000 as a bonus.
+    /// </summary>
+    /// <returns>2000</returns>
     protected override decimal CalculateBonus()
     {
         return 2000m;
     }
 
+    /// <summary>
+    /// Displays the information of the manager.
+    /// Extends the base class method to include manager-specific information.
+    /// </summary>
     public override void DisplayInfo()
     {
         // Aufruf der Methode der Basisklasse
